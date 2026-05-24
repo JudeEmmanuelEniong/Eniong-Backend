24b2fb0379eeb0a0b51262b9efb9ebf3d220afc2
JudeEmmanuelEniong <ejude248@gmail.com>
2026-05-24 23:13:52 +0800

    initial backend setup

---
+++ _helpers/send-email.ts
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer';
+
+export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT || '2525'),
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transporter.sendMail({ from, to, subject, html });
+}
