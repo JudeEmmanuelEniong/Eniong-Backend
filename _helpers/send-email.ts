e01ae0b02de42d3da4fc8251a2708bacb75d74fc
JudeEmmanuelEniong <ejude248@gmail.com>
2026-05-25 21:24:49 +0800

    fix: use correct EMAIL env variable names

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,12 +1,12 @@
 import nodemailer from 'nodemailer';
 
-export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_USER }: any) {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '2525'),
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT || '587'),
         auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
         }
     });
 
