4be85e7efee711bc5e5c22e203718bcc2fac15d0
JudeEmmanuelEniong <ejude248@gmail.com>
2026-05-25 21:40:09 +0800

    fix: add email timeout settings

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -4,6 +4,9 @@ export default async function sendEmail({ to, subject, html, from = process.env.
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: parseInt(process.env.EMAIL_PORT || '587'),
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
